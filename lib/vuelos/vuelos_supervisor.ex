92fc5ca9dac60e1d2308c7c1bb849479b6a1ff33
NicoMadd <nicomad.17@gmail.com>
2023-06-09 01:28:58 -0300

    # Agrego Vuelos DB como children de vuelos supervisor

--- lib/vuelos/vuelos_supervisor.ex
+++ lib/vuelos/vuelos_supervisor.ex
@@ -2,11 +2,11 @@ defmodule Vuelos.Supervisor do
   use Supervisor
 
   def start_link(init) do
-    Supervisor.start_link(__MODULE__, init)
+    Supervisor.start_link(__MODULE__, init, name: __MODULE__)
   end
 
   def init(_init_arg) do
-    children = [Vuelos.Worker]
+    children = [Vuelos.Worker, Vuelos.DB]
     opts = [strategy: :one_for_one]
 
     Supervisor.init(children, opts)
