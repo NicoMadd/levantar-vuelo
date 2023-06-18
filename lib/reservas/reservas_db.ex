e05e6768604c5c18bc4562786e6ba4e317dec4be
NicoMadd <nicomad.17@gmail.com>
2023-06-18 03:52:18 -0300

    # Refactor Reservas.DB - Puede fetchear una reserva en particular - Puede crear una reserva

--- lib/reservas/reservas_db.ex
+++ lib/reservas/reservas_db.ex
@@ -1,7 +1,7 @@
 defmodule Reservas.DB do
   use Agent
 
-  def start_link(initial_value) do
+  def start_link(_initial_value) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
@@ -9,10 +9,25 @@ defmodule Reservas.DB do
 
   # Crea una reserva a un vuelo de un usuario en particular.
   def crear_reserva(vuelo_id, usuario_id) do
+    # Generar id de reserva
+    reserva_id = App.Utils.generate_id()
+
+    # Crear reserva
+    reserva = {vuelo_id, usuario_id}
+
+    Agent.update(__MODULE__, fn reservas ->
+      Map.update(reservas, reserva_id, reserva, fn _ -> reserva end)
+    end)
+
+    reserva_id
   end
 
   # Dada una reserva la cancela. Considerar si debe ser baja logica o baja definitiva.
   def cancelar_reserva(reserva_id, usuario_id) do
+  end
+
+  def get(reserva_id) do
+    Agent.get(__MODULE__, fn reservas -> Map.get(reservas, reserva_id) end)
   end
 
   def get_all do
