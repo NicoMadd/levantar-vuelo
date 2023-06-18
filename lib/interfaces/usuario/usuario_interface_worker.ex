37446151790753c6e71c1d6df3cf54fb24ab02cc
NicoMadd <nicomad.17@gmail.com>
2023-06-18 04:51:15 -0300

    # Agrego notificacion de cierre de vuelo y reserva de asientos

--- lib/interfaces/usuario/usuario_interface_worker.ex
+++ lib/interfaces/usuario/usuario_interface_worker.ex
@@ -14,11 +14,21 @@ defmodule Usuario.Interface.Worker do
     Logger.info("Notificando a usuario: " <> "#{usuario_id}" <> " por el vuelo: " <> vuelo_id)
   end
 
-  def iniciar_reserva(usuario_id, vuelo_id) do
+  def notificar_cierre_vuelo(usuario_id, vuelo_id) do
+    Logger.info("Notificando a usuario: " <> "#{usuario_id}" <> " por el vuelo: " <> vuelo_id)
+  end
+
+  def iniciar_reserva(vuelo_id, usuario_id) do
     Logger.info(
       "Iniciando reserva para el vuelo " <> vuelo_id <> " para el usuario: #{usuario_id}"
     )
 
     Reservas.Worker.iniciar_reserva(:reservas_worker, vuelo_id, usuario_id)
+  end
+
+  def reservar_asientos(reserva_id, asientos) do
+    Logger.info("Seleccionando #{asientos} asientos para la reserva #{reserva_id}")
+
+    Reservas.Worker.reservar_asientos(:reservas_worker, reserva_id, asientos)
   end
 end
