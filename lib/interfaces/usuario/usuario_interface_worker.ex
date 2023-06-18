4ae2be28d08a0ef0ab364e93ccfac971d3347a3d
NicoMadd <nicomad.17@gmail.com>
2023-06-18 01:34:11 -0300

    # Agrego notificar vuelo para usuario

--- lib/interfaces/usuario/usuario_interface_worker.ex
+++ lib/interfaces/usuario/usuario_interface_worker.ex
@@ -1,5 +1,6 @@
 defmodule Usuario.Interface.Worker do
   use GenServer
+  require Logger
 
   def start_link(init) do
     GenServer.start_link(__MODULE__, init, name: :usuario_interface_worker)
@@ -7,5 +8,9 @@ defmodule Usuario.Interface.Worker do
 
   def init(_init_arg) do
     {:ok, []}
+  end
+
+  def notificar_nuevo_vuelo(usuario_id, vuelo_id) do
+    Logger.info("Notificando a usuario: " <> "#{usuario_id}" <> " por el vuelo: " <> vuelo_id)
   end
 end
