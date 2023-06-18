54bdf7361453a47eaff4976f77e11c73609be26e
NicoMadd <nicomad.17@gmail.com>
2023-06-18 01:53:31 -0300

    # Refactor alertas_db - Se crean alertas por mes, origen y destino - Se valida que los usuarios sean unicos en cada lista de alertas

--- lib/alertas/alertas_db.ex
+++ lib/alertas/alertas_db.ex
@@ -1,7 +1,7 @@
 defmodule Alertas.DB do
   use Agent
 
-  def start_link(initial_value) do
+  def start_link(_initial_value) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
@@ -9,7 +9,7 @@ defmodule Alertas.DB do
 
   def crear_alerta_por_mes(usuario_id, mes) do
     Agent.update(__MODULE__, fn s ->
-      Map.update(s, mes, [usuario_id], fn users -> [usuario_id | users] end)
+      Map.update(s, mes, [usuario_id], fn users -> Enum.uniq([usuario_id | users]) end)
     end)
   end
 
@@ -17,13 +17,35 @@ defmodule Alertas.DB do
   #   GenServer.call(pid, {:crear_por_fecha, {usuario_id, fecha}})
   # end
 
-  # def crear_alerta_por_origen(usuario_id, origen) do
-  #   GenServer.call(pid, {:crear_por_origen, {usuario_id, origen}})
-  # end
+  def crear_alerta_por_origen(usuario_id, origen) do
+    Agent.update(__MODULE__, fn s ->
+      Map.update(s, origen, [usuario_id], fn users -> Enum.uniq([usuario_id | users]) end)
+    end)
+  end
 
-  # def crear_alerta_por_destino(usuario_id, destino) do
-  #   GenServer.call(pid, {:crear_por_destino, {usuario_id, destino}})
-  # end
+  def crear_alerta_por_destino(usuario_id, destino) do
+    Agent.update(__MODULE__, fn s ->
+      Map.update(s, destino, [usuario_id], fn users -> Enum.uniq([usuario_id | users]) end)
+    end)
+  end
+
+  def remover_alerta_por_mes(usuario_id, mes) do
+    Agent.update(__MODULE__, fn s ->
+      Map.update(s, mes, [usuario_id], fn users -> List.delete(users, usuario_id) end)
+    end)
+  end
+
+  def remover_alerta_por_origen(usuario_id, origen) do
+    Agent.update(__MODULE__, fn s ->
+      Map.update(s, origen, [usuario_id], fn users -> List.delete(users, usuario_id) end)
+    end)
+  end
+
+  def remover_alerta_por_destino(usuario_id, destino) do
+    Agent.update(__MODULE__, fn s ->
+      Map.update(s, destino, [usuario_id], fn users -> List.delete(users, usuario_id) end)
+    end)
+  end
 
   def get_all do
     Agent.get(__MODULE__, fn s -> s end)
