c67a51b43b7ee7a10ceae59d767c4fb868d55f16
NicoMadd <nicomad.17@gmail.com>
2023-06-23 02:20:31 -0300

    # Notificacion de vuelos added

--- lib/vuelos/notifications/notification.ex
+++ lib/vuelos/notifications/notification.ex
@@ -11,22 +11,16 @@ defmodule Notification do
 
     month = date.month
 
-    [datetime, month, origen, destino]
-    |> Enum.map(fn dato -> {dato, Alertas.Registry.find_or_create_alerta(dato)} end)
-    |> Enum.each(fn {dato, {_, pid}} -> Alerta.notificar_usuarios(pid, {vuelo_id, dato}) end)
+    [{datetime, :fecha}, {month, :mes}, {origen, :origen}, {destino, :destino}]
+    |> Enum.map(fn {alerta_id, type} ->
+      {alerta_id, Alertas.Registry.find_or_create_alerta(alerta_id, type)}
+    end)
+    |> Enum.each(fn {alerta_id, {_, pid}} ->
+      Alerta.notificar_usuarios(pid, {vuelo_id, alerta_id})
+    end)
   end
 
   def notificar_cierre({vuelo_id, info}) do
-    Logger.info("Notificar #{vuelo_id}")
-
-    {_, _, datetime, origen, destino, _} = info
-
-    date = DateTime.to_date(datetime)
-
-    month = date.month
-
-    [datetime, month, origen, destino]
-    |> Enum.map(fn dato -> {dato, Alertas.Registry.find_or_create_alerta(dato)} end)
-    |> Enum.each(fn {dato, {_, pid}} -> Alerta.notificar_usuarios(pid, {vuelo_id, dato}) end)
+    Logger.info("Notificar cierre #{vuelo_id}")
   end
 end
