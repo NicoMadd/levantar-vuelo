c7552d346fe25f2c78ffdd190afd95da06e1cd43
NicoMadd <nicomad.17@gmail.com>
2023-06-24 13:37:16 -0300

    # Limpio interfaz de aerolinea

--- lib/interfaces/aerolinea/aerolinea.ex
+++ lib/interfaces/aerolinea/aerolinea.ex
@@ -9,31 +9,4 @@ defmodule Aerolinea do
   def init(_init_arg) do
     {:ok, []}
   end
-
-  def publicar_vuelo() do
-    Logger.info("Usuario publica vuelo")
-
-    Vuelos.DynamicSupervisor.publicar_vuelo(
-      "Boeing",
-      10,
-      ~U[2023-06-18 00:00:00Z],
-      "Buenos Aires",
-      "Montevideo",
-      3
-    )
-  end
-
-  def publicar_vuelo_largo() do
-    Logger.info("Usuario publica vuelo")
-
-    Vuelos.Worker.publicar_vuelo(
-      :vuelos_worker,
-      "Boeing",
-      10,
-      ~U[2023-06-18 00:00:00Z],
-      "Buenos Aires",
-      "Montevideo",
-      500
-    )
-  end
 end
