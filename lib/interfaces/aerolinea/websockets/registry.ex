8e9d70ddf39c970e57b2a5dd43ff0e1d0b5f1bfa
NicoMadd <nicomad.17@gmail.com>
2023-07-18 22:15:16 -0300

    # Agrego registries de websockets

---
+++ lib/interfaces/aerolinea/websockets/registry.ex
@@ -0,0 +1,21 @@
+defmodule Aerolinea.Websocket.Registry do
+  require Logger
+
+  def start_link(_init) do
+    Registry.start_link(keys: :unique, name: __MODULE__)
+  end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  def init(_init_arg) do
+    {:ok, []}
+  end
+end
