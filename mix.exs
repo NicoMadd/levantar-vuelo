17380b5055359a895b394083030f6bc30cff1ca1
NicoMadd <nicomad.17@gmail.com>
2023-07-27 00:43:58 -0300

    deps update

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule LevantarVuelo.MixProject do
       {:cowboy, "~> 2.6"},
       {:plug_cowboy, "~> 2.6"},
       {:jason, "~> 1.3"},
-      {:libcluster, "~> 3.3"}
+      {:libcluster, "~> 3.3"},
+      {:ksuid, "~> 0.1.2"}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
