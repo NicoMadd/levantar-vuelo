969d5d0a8fe033fe8734fa9c08017d0bf9ca6d2d
NicoMadd <nicomad.17@gmail.com>
2023-07-26 22:49:01 -0300

    Algoritmo seleccionador de master agregado

--- lib/node/replica_manager/replica_manager.ex
+++ lib/node/replica_manager/replica_manager.ex
@@ -12,9 +12,12 @@ defmodule Replica.Manager do
   def init(_args) do
     # Get timestamp for identifier
     identifier = Ksuid.generate()
-    IO.puts(identifier)
 
-    {:ok, {identifier, {[]}}}
+    IO.puts(
+      "Nodo " <> Atom.to_string(Node.self()) <> " levantado con identificador " <> identifier
+    )
+
+    {:ok, {identifier, {identifier, []}}}
   end
 
   def start_link(_) do
@@ -23,14 +26,27 @@ defmodule Replica.Manager do
 
   @doc """
   
-  Neighbour: {node, nb_timestamp}
+  Neighbour: {node, nb_identifier}
   """
-  def handle_call({:add_neighbour, neighbour_node}, _from, {identifier, neighbours}) do
-    {:reply, :ok, {identifier, [neighbour_node | neighbours]}}
+  def handle_call({:add_neighbour, new_neighbour}, _from, {identifier, {_old_master, neighbours}}) do
+    # Check if node is the new master ordering the identifier
+    master = select_master(identifier, new_neighbour, neighbours)
+
+    if(master == identifier) do
+      IO.puts("Este nodo es el elegido master")
+    else
+      IO.puts("Nuevo master " <> master)
+    end
+
+    {:reply, :ok, {identifier, {master, [new_neighbour | neighbours]}}}
   end
 
   def handle_call({:remove_neighbour, neighbour_node}, _from, {identifier, neighbours}) do
     {:reply, :ok, {identifier, List.delete(neighbours, neighbour_node)}}
+  end
+
+  def handle_call(:get_identifier, _from, {identifier, context}) do
+    {:reply, identifier, {identifier, context}}
   end
 
   def handle_call(:ping, _from, state) do
@@ -54,5 +70,24 @@ defmodule Replica.Manager do
     GenServer.call(pid, {:remove_neighbour, neighbour_node})
   end
 
+  def get_identifier(pid) do
+    GenServer.call(pid, :get_identifier)
+  end
+
   # Private functions
+
+  defp select_master(own_identifier, {_node, new_identifier}, neighbours) do
+    selected =
+      neighbours
+      |> Enum.map(fn {_node, id} -> id end)
+      |> Enum.concat([new_identifier, own_identifier])
+      |> Enum.uniq()
+      |> Enum.sort()
+      |> Enum.fetch(0)
+
+    case selected do
+      {:ok, master} -> master
+      :error -> new_identifier
+    end
+  end
 end
