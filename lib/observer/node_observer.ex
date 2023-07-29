02894d181e03e7316d865029b53fb4ad8ad615c1
NicoMadd <nicomad.17@gmail.com>
2023-07-28 22:38:59 -0300

    Conexion automatica entre DeltaCRDTs de distinto nodo

--- lib/observer/node_observer.ex
+++ lib/observer/node_observer.ex
@@ -51,6 +51,7 @@ defmodule NodeObserver do
       %{node_affected: node},
       %{}
     )
+
     # Supervisors
     set_members(Entidades.Usuario.DynamicSupervisor)
     set_members(Alertas.DynamicSupervisor)
@@ -63,10 +64,13 @@ defmodule NodeObserver do
     set_members(Entidades.Usuario.Registry)
     set_members(Alertas.Registry)
 
+    # Delta CRDT
+    State.Manager.refresh_neighbours()
+
     {:noreply, state}
   end
 
-   defp set_members(name) do
+  defp set_members(name) do
     members = Enum.map([Node.self() | Node.list()], &{name, &1})
 
     :ok = Horde.Cluster.set_members(name, members)
--- lib/state_manager/state_manager.ex
+++ lib/state_manager/state_manager.ex
@@ -1,10 +1,32 @@
 defmodule State.Manager do
   def start_link(_) do
-    DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 3)
+    DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, name: Node.self(), sync_interval: 3)
   end
 
+  def get_crtd() do
+  end
+
+  @doc """
+  Cada neighbour basicamente se compone de el nombre del DeltaCRDT en el nodo al que invoca y ese mismo nodo
+  
+  `{crdt_name, node_name}`
+  
+  Esto es para que despues sepa a que nodo apuntar ya que cada DeltaCRDT en cada nodo es unico y nombrado segun el nombre del nodo.
+  """
   def set_neighbours(crdt, neighbours) do
     DeltaCrdt.set_neighbours(crdt, neighbours)
+  end
+
+  @doc """
+  Aca directamente se puede usar esta funcion sin argumentos ya que se invoca al DeltaCRDT nombrado como el nodo mismo
+  y se agregan los nodos conectados al nodo en cuestion.
+  """
+  def refresh_neighbours() do
+    neighbours =
+      Node.list()
+      |> Enum.map(fn n -> {n, n} end)
+
+    State.Manager.set_neighbours(Node.self(), neighbours)
   end
 
   def save_state(crdt, key, value) do
