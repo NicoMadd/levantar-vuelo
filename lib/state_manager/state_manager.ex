47f45a4621bd14cc739a2ec39c5676010fafc4dd
NicoMadd <nicomad.17@gmail.com>
2023-07-28 21:27:22 -0300

    agrego DeltaCRDT manager base

--- lib/state_manager/state_manager.ex
+++ lib/state_manager/state_manager.ex
@@ -1,11 +1,17 @@
 defmodule State.Manager do
-  use Agent
-
   def start_link(_) do
-    Agent.start_link(fn -> init() end, name: __MODULE__)
+    DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 3)
   end
 
-  def init do
-    Map.new()
+  def set_neighbours(crdt, neighbours) do
+    DeltaCrdt.set_neighbours(crdt, neighbours)
+  end
+
+  def save_state(crdt, key, value) do
+    DeltaCrdt.put(crdt, key, value)
+  end
+
+  def delete_state(crdt, key) do
+    DeltaCrdt.delete(crdt, key)
   end
 end
