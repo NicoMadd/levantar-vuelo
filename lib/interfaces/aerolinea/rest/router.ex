01381c2f01d5104ab901770fd2752bb10f27dcf2
larreaf <fedeml.95@gmail.com>
2023-07-29 02:42:46 -0300

    # Agrego endpoint para obtener info de vuelo

--- lib/interfaces/aerolinea/rest/router.ex
+++ lib/interfaces/aerolinea/rest/router.ex
@@ -28,12 +28,44 @@ defmodule Aerolinea.Router do
     datetime = validar_fecha(conn.body_params["fecha"])
     tiempo_limite = conn.body_params["limite"]
 
-    Vuelos.DynamicSupervisor.publicar_vuelo(tipo_avion, datetime, origen, destino, tiempo_limite)
-    send_resp(conn, 200, "OK")
+    {:ok, vuelo_id} = Vuelos.DynamicSupervisor.publicar_vuelo(tipo_avion, datetime, origen, destino, tiempo_limite)
+
+    send_resp(conn, 201, "{\"id\": \"#{vuelo_id}\"}")
+  end
+
+  get "/vuelos/:vuelo_id" do
+    # TODO testear validacion de parametros
+    case Vuelos.Registry.find(vuelo_id) do
+      [{pid, _}] -> send_resp(conn, 200, obtener_datos_vuelo(pid))
+      [] -> send_resp(conn, 404, "")
+    end
   end
 
   match _ do
     send_resp(conn, 404, "Not Found")
+  end
+
+  defp obtener_datos_vuelo(pid_vuelo) do
+    vuelo_info_struct = GenServer.call(pid_vuelo, :info)
+
+    asientos = Enum.map(vuelo_info_struct.asientos, fn a ->
+      %{
+        numero: a.numero,
+        disponible?: a.disponible?,
+        pasajero: a.pasajero
+      }
+    end)
+
+    vuelo_map_info = %{
+      tipo_avion: vuelo_info_struct.tipo_avion,
+      fecha_hora_despegue: vuelo_info_struct.fecha_hora_despegue,
+      origen: vuelo_info_struct.origen,
+      destino: vuelo_info_struct.destino,
+      tiempo_oferta: vuelo_info_struct.tiempo_oferta,
+      asientos: asientos
+    }
+    
+    Jason.encode(vuelo_map_info) |> elem(1)
   end
 
   defp validar_tipo(tipo) do
